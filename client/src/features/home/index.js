aae397995c8aaca2a02e67ca1aa61338dbbb6b8f
Steven Wu <swu@nxtcloud.com>
2018-06-14 10:28:32 +0800

    removed saga folder

--- client/src/features/home/index.js
+++
@@ -1,4 +0,0 @@
-export { default as App } from './App';
-export { default as DefaultPage } from './DefaultPage';
-export { default as DataTable } from './DataTable';
-export { default as FbContainer } from './FbContainer';
--- client/tests/features/home/DataTable.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { DataTable } from '../../../src/features/home';
-
-it('renders node with correct class name', () => {
-  const renderedComponent = shallow(<DataTable />);
-  expect(renderedComponent.find('.home-data-table').length).toBe(1);
-});
